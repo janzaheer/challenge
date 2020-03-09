7dbd495f5a6829dffea2b4ed88207ee743676bdb
Zaheer Jan <zaheerjanbadini@gmail.com>
2020-03-09 16:18:04 +0500

    heroku configurations

--- challenge/settings.py
+++ challenge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#5v9b8)xfg&3%d*1qx$w(a_13jmx%(wq98vjl6effh78)k5i^j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['explor-challeng.herokuapp.com', '*']
 
 
 # Application definition
@@ -57,13 +57,17 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
+            ]
         },
     },
 ]
